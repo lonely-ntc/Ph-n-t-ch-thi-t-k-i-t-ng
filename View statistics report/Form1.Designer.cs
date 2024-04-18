e47b660df6cac31c5702157810a3cbf896d703e4
lonely-ntc <128463086+lonely-ntc@users.noreply.github.com>
2024-04-18 12:14:22 +0700

    Add files via upload

---
+++ View statistics report/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ViewStatisticsReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.rdbtnBaoCaoND = new System.Windows.Forms.RadioButton();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtKetQua = new System.Windows.Forms.RichTextBox();
+            this.rdbtnBaoCaoSach = new System.Windows.Forms.RadioButton();
+            this.rdbtnBaoCaoDT = new System.Windows.Forms.RadioButton();
+            this.btnViewReport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdbtnBaoCaoND
+            // 
+            this.rdbtnBaoCaoND.AutoSize = true;
+            this.rdbtnBaoCaoND.Checked = true;
+            this.rdbtnBaoCaoND.Location = new System.Drawing.Point(114, 232);
+            this.rdbtnBaoCaoND.Name = "rdbtnBaoCaoND";
+            this.rdbtnBaoCaoND.Size = new System.Drawing.Size(148, 20);
+            this.rdbtnBaoCaoND.TabIndex = 0;
+            this.rdbtnBaoCaoND.TabStop = true;
+            this.rdbtnBaoCaoND.Text = "Báo cáo người dùng";
+            this.rdbtnBaoCaoND.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(272, 50);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(337, 307);
+            this.txtKetQua.TabIndex = 2;
+            this.txtKetQua.Text = "";
+            // 
+            // rdbtnBaoCaoSach
+            // 
+            this.rdbtnBaoCaoSach.AutoSize = true;
+            this.rdbtnBaoCaoSach.Location = new System.Drawing.Point(114, 258);
+            this.rdbtnBaoCaoSach.Name = "rdbtnBaoCaoSach";
+            this.rdbtnBaoCaoSach.Size = new System.Drawing.Size(111, 20);
+            this.rdbtnBaoCaoSach.TabIndex = 0;
+            this.rdbtnBaoCaoSach.TabStop = true;
+            this.rdbtnBaoCaoSach.Text = "Báo cáo sách";
+            this.rdbtnBaoCaoSach.UseVisualStyleBackColor = true;
+            // 
+            // rdbtnBaoCaoDT
+            // 
+            this.rdbtnBaoCaoDT.AutoSize = true;
+            this.rdbtnBaoCaoDT.Location = new System.Drawing.Point(114, 284);
+            this.rdbtnBaoCaoDT.Name = "rdbtnBaoCaoDT";
+            this.rdbtnBaoCaoDT.Size = new System.Drawing.Size(140, 20);
+            this.rdbtnBaoCaoDT.TabIndex = 0;
+            this.rdbtnBaoCaoDT.TabStop = true;
+            this.rdbtnBaoCaoDT.Text = "Báo cáo doanh thu";
+            this.rdbtnBaoCaoDT.UseVisualStyleBackColor = true;
+            // 
+            // btnViewReport
+            // 
+            this.btnViewReport.Location = new System.Drawing.Point(114, 322);
+            this.btnViewReport.Name = "btnViewReport";
+            this.btnViewReport.Size = new System.Drawing.Size(152, 35);
+            this.btnViewReport.TabIndex = 3;
+            this.btnViewReport.Text = "Xem báo cáo";
+            this.btnViewReport.UseVisualStyleBackColor = true;
+            this.btnViewReport.Click += new System.EventHandler(this.btnViewReport_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nội dung báo cáo:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(80, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(563, 376);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Xem báo cáo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 55);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Quay lại trang chủ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::View_statistics_report.Properties.Resources.OIP__3_;
+            this.pictureBox1.Location = new System.Drawing.Point(50, 395);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(34, 194);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 462);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnViewReport);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.rdbtnBaoCaoDT);
+            this.Controls.Add(this.rdbtnBaoCaoSach);
+            this.Controls.Add(this.rdbtnBaoCaoND);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbtnBaoCaoND;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox txtKetQua;
+        private System.Windows.Forms.RadioButton rdbtnBaoCaoSach;
+        private System.Windows.Forms.RadioButton rdbtnBaoCaoDT;
+        private System.Windows.Forms.Button btnViewReport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ View statistics report/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+    namespace ViewStatisticsReport
+    {
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            rdbtnBaoCaoND.Checked = false;
+            rdbtnBaoCaoSach.Checked = false;
+            rdbtnBaoCaoDT.Checked = false;
+        }
+
+           
+
+            private void DisplayReports(List<string> selectedReportTypes)
+            {
+                foreach (string reportType in selectedReportTypes)
+                {
+                    switch (reportType)
+                    {
+                        case "Báo cáo người dùng":
+                            DisplayUserReport();
+                            break;
+                        case "Báo cáo sách":
+                            DisplayBookReport();
+                            break;
+                        case "Báo cáo doanh thu":
+                            DisplayRevenueReport();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+        private void DisplayUserReport()
+        {
+            txtKetQua.AppendText("Báo cáo người dùng: Tổng số người dùng: 1,5 triệu người dùng (Đã có hơn 1,5 triệu người dùng đăng ký app)\nSố lượng người dùng mới: 300.000 người dùng/năm (Đã có hơn 300.000 người dùng mới trong 1 năm)\n...\n\n");
+        }
+
+        private void DisplayBookReport()
+        {
+            txtKetQua.AppendText("Báo cáo sách:\nTổng số sách: 4tr+ ( Hơn 4tr sách có trong hệ thống của app)\\n\r\n\r\nSố lượng sách bán ra: 1tr+/1year ( Hơn 1tr cuốn sách được bán ra trong 1 năm)\\n\r\n\r\nDoanh thu bán sách: 20 tỷ VNĐ/1year (Doanh thu bán sách đạt 20 tỷ đồng trong 1 năm)\\n\r\n\r\nSách bán chạy nhất:\\n\r\n\r\nTop 1: \"Tên sách bán chạy nhất\" (Tác giả) - 100k bản\\n\r\n\r\nTop 2: \"Tên sách bán chạy thứ 2\" (Tác giả) - 80k bản\\n\r\n\r\nTop 3: \"Tên sách bán chạy thứ 3\" (Tác giả) - 70k bản\\n\r\n\r\nPhân tích theo thể loại:\\n\r\n\r\nVăn học: 40% (40% doanh thu bán sách thuộc về thể loại văn học)\\n\r\n\r\nKỹ năng sống: 30% (30% doanh thu bán sách thuộc về thể loại kỹ năng sống)\\n\r\n\r\nKinh doanh: 20% (20% doanh thu bán sách thuộc về thể loại kinh doanh)\\n\r\n\r\nKhác: 10% (10% doanh thu bán sách thuộc về các thể loại khác)\\n\r\n\r\nPhân tích theo tác giả:\\n\r\n\r\nTác giả 1: 200k bản (Tác giả 1 bán được 200k bản sách)\\n\r\n\r\nTác giả 2: 150k bản (Tác giả 2 bán được 150k bản sách)\\n\r\n\r\nTác giả 3: 100k bản (Tác giả 3 bán được 100k bản sách)\\n\r\n\r\nLợi nhuận gộp: 5 tỷ VNĐ/1year (Lợi nhuận gộp đạt 5 tỷ đồng trong 1 năm)\\n\r\n\r\nTỷ suất lợi nhuận: 25% (Tỷ suất lợi nhuận đạt 25%)\\n\r\n\r\nPhân tích theo chi phí:\\n\r\n\r\nChi phí nhân viên: 2 tỷ VNĐ/1year\\n\r\n\r\nChi phí marketing: 1 tỷ VNĐ/1year\\n\r\n\r\nChi phí vận chuyển: 500 triệu VNĐ/1year\\n\r\n\r\nChi phí khác: 500 triệu VNĐ/1year\\n");
+        }
+
+        private void DisplayRevenueReport()
+        {
+            txtKetQua.AppendText("Báo cáo doanh thu: Doanh thu theo kênh bán hàng:\nOnline: 15 tỷ VNĐ/1year (75% doanh thu bán sách đến từ kênh bán hàng online)\nOffline: 5 tỷ VNĐ/1year (25% doanh thu bán sách đến từ kênh bán hàng offline)\n...\n\n");
+        }
+
+        private void btnViewReport_Click_1(object sender, EventArgs e)
+        {
+            List<string> selectedReportTypes = GetSelectedReportTypes();
+
+            if (selectedReportTypes.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn ít nhất một loại báo cáo!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DisplayReports(selectedReportTypes);
+        }
+
+
+        private List<string> GetSelectedReportTypes()
+        {
+            List<string> selectedReportTypes = new List<string>();
+
+            if (rdbtnBaoCaoND.Checked)
+            {
+                selectedReportTypes.Add("Báo cáo người dùng");
+            }
+
+            if (rdbtnBaoCaoSach.Checked)
+            {
+                selectedReportTypes.Add("Báo cáo sách");
+            }
+
+            if (rdbtnBaoCaoDT.Checked)
+            {
+                selectedReportTypes.Add("Báo cáo doanh thu");
+            }
+
+            return selectedReportTypes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
