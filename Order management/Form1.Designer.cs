9e4abc9b91df55db9a208c75ce6e6a6198864196
lonely-ntc <128463086+lonely-ntc@users.noreply.github.com>
2024-04-18 12:15:06 +0700

    Add files via upload

---
+++ Order management/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Order_management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listDonHang = new System.Windows.Forms.ListBox();
+            this.btnXacNhanDon = new System.Windows.Forms.Button();
+            this.btnCapNhatTrangThai = new System.Windows.Forms.Button();
+            this.btnHuyDon = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listDonHang
+            // 
+            this.listDonHang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listDonHang.FormattingEnabled = true;
+            this.listDonHang.ItemHeight = 20;
+            this.listDonHang.Location = new System.Drawing.Point(62, 141);
+            this.listDonHang.Name = "listDonHang";
+            this.listDonHang.Size = new System.Drawing.Size(812, 84);
+            this.listDonHang.TabIndex = 0;
+            // 
+            // btnXacNhanDon
+            // 
+            this.btnXacNhanDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXacNhanDon.Location = new System.Drawing.Point(218, 163);
+            this.btnXacNhanDon.Name = "btnXacNhanDon";
+            this.btnXacNhanDon.Size = new System.Drawing.Size(125, 40);
+            this.btnXacNhanDon.TabIndex = 1;
+            this.btnXacNhanDon.Text = "Xác nhận đơn";
+            this.btnXacNhanDon.UseVisualStyleBackColor = true;
+            this.btnXacNhanDon.Click += new System.EventHandler(this.btnXacNhanDon_Click);
+            // 
+            // btnCapNhatTrangThai
+            // 
+            this.btnCapNhatTrangThai.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapNhatTrangThai.Location = new System.Drawing.Point(395, 163);
+            this.btnCapNhatTrangThai.Name = "btnCapNhatTrangThai";
+            this.btnCapNhatTrangThai.Size = new System.Drawing.Size(125, 40);
+            this.btnCapNhatTrangThai.TabIndex = 1;
+            this.btnCapNhatTrangThai.Text = "Cập nhật trạng thái";
+            this.btnCapNhatTrangThai.UseVisualStyleBackColor = true;
+            this.btnCapNhatTrangThai.Click += new System.EventHandler(this.btnCapNhatTrangThai_Click);
+            // 
+            // btnHuyDon
+            // 
+            this.btnHuyDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuyDon.Location = new System.Drawing.Point(563, 163);
+            this.btnHuyDon.Name = "btnHuyDon";
+            this.btnHuyDon.Size = new System.Drawing.Size(125, 40);
+            this.btnHuyDon.TabIndex = 1;
+            this.btnHuyDon.Text = "Hủy đơn";
+            this.btnHuyDon.UseVisualStyleBackColor = true;
+            this.btnHuyDon.Click += new System.EventHandler(this.btnHuyDon_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnHuyDon);
+            this.groupBox1.Controls.Add(this.btnCapNhatTrangThai);
+            this.groupBox1.Controls.Add(this.btnXacNhanDon);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(37, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(843, 220);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Order management";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(673, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 72);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Danh sách đơn đã xác nhận";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(168, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 49);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Quay lại trang chủ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Order_management.Properties.Resources.OIP__3_;
+            this.pictureBox2.Location = new System.Drawing.Point(62, 348);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Order_management.Properties.Resources.OIP__5_;
+            this.pictureBox1.Location = new System.Drawing.Point(418, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 421);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listDonHang);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Order management";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listDonHang;
+        private System.Windows.Forms.Button btnXacNhanDon;
+        private System.Windows.Forms.Button btnCapNhatTrangThai;
+        private System.Windows.Forms.Button btnHuyDon;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Order management/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Order_management
+{
+    public partial class Form1 : Form
+    {
+        private List<DonHang> danhSachDonHang = new List<DonHang>
+        {
+           new DonHang("Bắt Đầu Từ Con Số 0", "Hồ Chí Minh", "đang giao"),
+            new DonHang("Đắc Nhân Tâm", "Đà Nẵng", "đang chờ xử lý"),
+            new DonHang("Lược Sử Vạn Vật", "Cần Thơ", "trên đường đi"),
+            new DonHang("Nhà Giả Kim", "Hà Nội", "đã giao"),
+            new DonHang(" Sapiens: Lược Sử Loài Người", "Hải Phòng", "đang chờ xử lý"),
+            new DonHang("Tuổi Trẻ Đáng Giá Bao Nhiêu?", "Bình Dương", "đã giao"),
+            new DonHang(" Muôn Kiếp Nhân Sinh", "Đồng Nai", "đang giao"),
+            new DonHang("Lối Sống Tối Giản Của Người Nhật", "Khánh Hòa", "trên đường đi"),
+            new DonHang("Cha Giàu Cha Nghèo", "Quảng Nam", "đang chờ xử lý"),
+            new DonHang(" Tôi Tự Học", "Lâm Đồng", "đã giao"),
+
+
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadDataToListBox();
+        }
+
+        private void LoadDataToListBox()
+        {
+            foreach (var donHang in danhSachDonHang)
+            {
+                listDonHang.Items.Add(donHang);
+            }
+        }
+
+
+        private void btnXacNhanDon_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc muốn xác nhận đơn hàng này?", "Xác nhận đơn hàng", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                MessageBox.Show("Xác nhận đơn hàng thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+            }
+        }
+        private void btnCapNhatTrangThai_Click(object sender, EventArgs e)
+        {
+            string[] trangThaiMoi = { "Đang giao", "Đang chờ xử lý", "Trên đường đi" };
+            string trangThaiCu = ((DonHang)listDonHang.SelectedItem).TrangThai;
+
+            DialogResult dialogResult = MessageBox.Show("Chọn trạng thái mới cho đơn hàng:", "Cập nhật trạng thái", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.OK)
+            {
+                
+                
+            }
+        }
+
+        private void btnHuyDon_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc muốn hủy đơn hàng này?", "Xác nhận hủy đơn hàng", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                danhSachDonHang.Remove((DonHang)listDonHang.SelectedItem);
+                RefreshListBox();
+            }
+        }
+        private void RefreshListBox()
+        {
+            listDonHang.Items.Clear();
+            foreach (var donHang in danhSachDonHang)
+            {
+                listDonHang.Items.Add(donHang);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class DonHang
+    {
+        public string TenSach { get; set; }
+        public string DiaDiem { get; set; }
+        public string TrangThai { get; set; }
+
+        public DonHang(string tenSach, string diaDiem, string trangThai)
+        {
+            TenSach = tenSach;
+            DiaDiem = diaDiem;
+            TrangThai = trangThai;
+        }
+
+        public override string ToString()
+        {
+            return $"Tên sách: {TenSach} - Địa điểm: {DiaDiem} - Trạng thái: {TrangThai}";
+        }
+    }
+}
